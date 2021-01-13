5bbd41c741d9573c2c9a5c8eb0326026b41432fb
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2021-01-13 21:10:15 +0100

    Import random, add width and height, changed width and height in generate_random_mine_locations

--- minesweeper.py
+++ minesweeper.py
@@ -1,8 +1,13 @@
+import random
+
+width = 10
+height = width
+
 def generate_random_mine_locations(where_user_clicked, how_many_mines_user_wants):
     mine_locations = []
     while len(mine_locations) < how_many_mines_user_wants:
-        x = random.randint(0, WIDTH - 1)
-        y = random.randint(0, HEIGHT - 1)
+        x = random.randint(WIDTH)
+        y = random.randint(HEIGHT)
         if (x, y) != where_user_clicked and (x, y) not in mine_locations:
             mine_locations.append((x, y))
     return mine_locations
