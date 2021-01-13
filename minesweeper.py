dd708450eacdd895f0dd885e019b36e61f8c8b51
Arrinao <arrinao@email.cz>
2021-01-13 21:16:42 +0100

    changed ranrange WIDTH and HEIGHT

--- minesweeper.py
+++ minesweeper.py
@@ -1,8 +1,8 @@
 def generate_random_mine_locations(where_user_clicked, how_many_mines_user_wants):
     mine_locations = []
     while len(mine_locations) < how_many_mines_user_wants:
-        x = random.randint(0, WIDTH - 1)
-        y = random.randint(0, HEIGHT - 1)
+        x = random.randrange(WIDTH)
+        y = random.randrange(HEIGHT)
         if (x, y) != where_user_clicked and (x, y) not in mine_locations:
             mine_locations.append((x, y))
     return mine_locations
